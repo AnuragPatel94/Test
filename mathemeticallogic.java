865f32ae5a81c55db8d016313d5bbfa7eab09317
Anurag <apatel665@gmail.com>
2025-12-10 14:56:11 +0530

    data changed and number reverse code removed

--- mathemeticallogic.java
+++ mathemeticallogic.java
@@ -2,19 +2,14 @@ public class mathemeticallogic  {
         public static void main(String[] args) {
 		
 //Swap 2 number using third variable
-		int a=100, b=200;
+		int a=10000, b=20000;
 		System.out.println("Before swap..."+a+"  "+b);
 		int t=a;
 		    a=b;
 		    b=t;
 	    System.out.println("After swap..."+a+"  "+b);  
 
-//Reverse Number using stringbuilder class
 
-	      StringBuilder sbl=new StringBuilder();
-	      sbl.append(123456789);
-	      StringBuilder rev =sbl.reverse();
-	      System.out.println("Reverse String is:"+rev);
 	
 	
 	}}
